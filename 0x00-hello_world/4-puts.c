c8066a0a957bde4bc06d4603e6539fd56d0bafde
Melakew <Del.neg24@gmail.com>
2023-03-16 14:21:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that uses function puts
+ * return: 0 (Success)
+ * /
+ 
+ int main(void)
+ {
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
+ }
